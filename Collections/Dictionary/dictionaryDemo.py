e986346130134e99b70923be45c1d134db77bfae
Ashok <ashok.karale@outlook.com>
2021-04-29 21:53:53 +0530

    Added Collections - Set & Dictionary code

---
+++ Collections/Dictionary/dictionaryDemo.py
@@ -0,0 +1,6 @@
+crew_details = {"Pilot": "Kumar",
+                "Co-pilot": "Raghav",
+                "Head-Strewardess": "Malini",
+                "Stewardess": "Mala"}
+for key, value in crew_details.items():
+    print(key, ":", value)
---
+++ Collections/Set/setDemo.py
@@ -0,0 +1,10 @@
+passengers_list=["George", "Annie", "Jack", "Annie", "Henry", "Helen", "Maria", "George", "Jack", "Remo"]
+
+uniqueList=set(passengers_list)
+print("Original List")
+for i in passengers_list:
+    print(i)
+print("***********************************************")
+print("Unique List")
+for i in uniqueList:
+    print(i)
